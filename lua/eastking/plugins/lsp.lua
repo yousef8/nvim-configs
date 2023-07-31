04386fd9b1e443371a36f42626affc4f03ecbd41
Yousef Elsayed <yousef4511@outlook.com>
2023-08-01 02:28:22 +0300

    Format on save on all formatting capable lsp

--- lua/eastking/plugins/lsp.lua
+++ lua/eastking/plugins/lsp.lua
@@ -13,10 +13,10 @@ return
         require("mason").setup()
 
         require("mason-lspconfig").setup {
-            ensure_installed = { "lua_ls", "clangd" },
+            ensure_installed = { "lua_ls", "clangd", },
         }
 
-        local function on_attach()
+        local function on_attach(client, bufnr)
             vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = 0 })
             vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, { buffer = 0 })
             vim.keymap.set("n", "<leader>gt", vim.lsp.buf.type_definition, { buffer = 0 })
@@ -24,6 +24,17 @@ return
             vim.keymap.set("n", "[d", vim.diagnostic.goto_next, { buffer = 0 })
             vim.keymap.set("n", "]d", vim.diagnostic.goto_prev, { buffer = 0 })
             vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { buffer = 0 })
+
+            local format_on_save = vim.api.nvim_create_augroup('formatOnSave', { clear = false })
+
+            if client.supports_method("textDocument/formatting") then
+                vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+                    buffer = bufnr,
+                    group = format_on_save,
+                    callback = function() vim.lsp.buf.format() end,
+                    desc = "Format on save a file with formatting capable attached language server"
+                })
+            end
         end
 
         require("mason-lspconfig").setup_handlers {
