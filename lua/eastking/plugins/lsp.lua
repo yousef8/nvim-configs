0f6fac499217b1867f35bcaae8c3529796276231
Yousef Elsayed <yousef4511@outlook.com>
2023-08-01 18:00:43 +0300

    Configure lua_ls to idnetify 'vim' as global var

--- lua/eastking/plugins/lsp.lua
+++ lua/eastking/plugins/lsp.lua
@@ -46,6 +46,27 @@ return
                     capabilities = capabilities
                 }
             end,
+            ["lua_ls"] = function()
+                local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+                require("lspconfig").lua_ls.setup {
+                    on_attach = on_attach,
+                    capabilities = capabilities,
+                    settings = {
+                        Lua = {
+                            runtime = {
+                                version = 'LuaJIT',
+                            },
+                            diagnostics = {
+                                globals = { 'vim' },
+                            },
+                            workspace = {
+                                library = vim.api.nvim_get_runtime_file("", true),
+                            },
+                        }
+                    }
+                }
+            end,
         }
     end,
 }
