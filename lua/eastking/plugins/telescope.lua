f7b9d70865f5082860add415f7f722e0a89f5338
Yousef Elsayed <yousef4511@outlook.com>
2023-08-03 00:48:51 +0300

    Add telescope-ui-select extension
    Also, add keymapping for diagnostics picker in telescope

--- lua/eastking/plugins/telescope.lua
+++ lua/eastking/plugins/telescope.lua
@@ -15,29 +15,41 @@ return
         {
             "nvim-tree/nvim-web-devicons",
         },
+        {
+            'nvim-telescope/telescope-ui-select.nvim',
+        },
     },
 
     config = function(_, _)
         local actions = require "telescope.actions"
         local telescope = require('telescope')
+        local action_state = require 'telescope.actions.state'
 
         vim.keymap.set('n', '<leader>ff',
-            "<cmd> lua require('telescope.builtin').find_files(require('telescope.themes').get_dropdown())<CR>",
+            "<cmd> lua require('telescope.builtin').find_files(require('telescope.themes').get_dropdown({winblend=10}))<CR>",
             {})
         vim.keymap.set('n', '<leader>fg',
-            "<cmd> lua require('telescope.builtin').live_grep(require('telescope.themes').get_dropdown())<CR>",
+            "<cmd> lua require('telescope.builtin').live_grep(require('telescope.themes').get_dropdown({winblend=10}))<CR>",
             {})
         vim.keymap.set('n', '<leader>fb',
-            "<cmd> lua require('telescope.builtin').buffers(require('telescope.themes').get_dropdown())<CR>",
+            "<cmd> lua require('telescope.builtin').buffers(require('telescope.themes').get_dropdown({winblend=10}))<CR>",
             {})
         vim.keymap.set('n', '<leader>fh',
-            "<cmd> lua require('telescope.builtin').help_tags(require('telescope.themes').get_dropdown())<CR>",
+            "<cmd> lua require('telescope.builtin').help_tags(require('telescope.themes').get_dropdown({winblend=10}))<CR>",
             {})
         vim.keymap.set('n', '<C-_>',
-            "<cmd> lua require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown())<CR>",
+            "<cmd> lua require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown({previewer=false, winblend=10}))<CR>",
+            {})
+        vim.keymap.set('n', '<leader>ld',
+            "<cmd> lua require('telescope.builtin').diagnostics(require('telescope.themes').get_dropdown({winblend=10, previewer=false}))<CR>",
             {})
 
         telescope.setup {
+            extensions = {
+                ["ui-select"] = {
+                    require('telescope.themes').get_dropdown({ winblend = 10 }),
+                },
+            },
             defaults = {
                 prompt_prefix = " ",
                 selection_caret = " ",
@@ -47,6 +59,9 @@ return
                     i = {
                         ["<C-j>"] = actions.move_selection_next,
                         ["<C-k>"] = actions.move_selection_previous,
+                        ["<c-a>"] = function()
+                            print(vim.inspect(action_state.get_selected_entry()))
+                        end,
                     }
                 }
             },
@@ -54,5 +69,6 @@ return
 
 
         telescope.load_extension('fzf')
+        telescope.load_extension("ui-select")
     end,
 }
