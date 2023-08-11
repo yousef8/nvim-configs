a11df202a8f0db15f30eeb576f00ff8003ff2390
Yousef Elsayed <yousef4511@outlook.com>
2023-08-11 19:53:04 +0300

    Add mini.bufremove plugin to properly delete a buf

---
+++ lua/eastking/plugins/bufremove.lua
@@ -0,0 +1,14 @@
+local M = {
+    'echasnovski/mini.bufremove',
+    version = '*',
+    config = true,
+}
+
+function M.config()
+    local Bufremove = require('mini.bufremove')
+
+    vim.keymap.set('n', '<leader>bd', Bufremove.delete, { desc = '[B]uffer [D]elete' })
+    Bufremove.setup {}
+end
+
+return M
